e302a6003b941bdd57666e85bd673f0bb738e71a
Nisa Dost <i.n.dost38@gmail.com>
2025-12-01 14:33:33 +0300

    create project

---
+++ TurkishVacuum/TurkishVacuum.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TurkishVacuum.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
